9d3393b325674654d98fd54a9407699bc3ff7e35
Oleg Laskov <olaskov@gmail.com>
2019-09-19 10:33:48 +0300

    Added vibrate

--- src/store.js
+++ src/store.js
@@ -4,11 +4,22 @@ import reducer from './reducers';
 
 const logMiddleware = ({ getState }) => (next) => (action) => {
     //console.log(action.type, getState());
+    if(action.type === '@@router/LOCATION_CHANGE'){
+        //console.log('vibrate');
+        vibrate(100);
+    }
     return next(action);
 };
 
+const vibrate = (val) => {
+    if("vibrate" in navigator)  return navigator.vibrate(val);
+    if("oVibrate" in navigator)  return navigator.oVibrate(val);
+    if("mozVibrate" in navigator)  return navigator.mozVibrate(val);
+    if("webkitVibrate" in navigator)  return navigator.webkitVibrate(val);
+}
+
 const store = createStore(reducer, composeWithDevTools(
-    applyMiddleware(logMiddleware)
-  ));
+        applyMiddleware(logMiddleware)
+    ));
 
 export default store;
